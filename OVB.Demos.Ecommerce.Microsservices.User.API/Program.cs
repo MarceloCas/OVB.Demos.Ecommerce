b9707d6a5327962d344b4ae60c6e3cc459d17b76
OtavioVB <otaviovb.developer@gmail.com>
2022-10-22 12:30:14 -0300

    fix: unnecessary libs

--- OVB.Demos.Ecommerce.Microsservices.User.API/Program.cs
+++ OVB.Demos.Ecommerce.Microsservices.User.API/Program.cs
@@ -1,51 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.OpenApi.Models;
-using System.Reflection;
 
 namespace OVB.Demos.Ecommerce.Microsservices.User.Api;
 
 public class Program
 {
-    public static void Main(string[] args)
+public static void Main(string[] args)
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddControllers();
+
+    builder.Services.AddApiVersioning(options =>
     {
-        var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddControllers();
+        options.AssumeDefaultVersionWhenUnspecified = true;
+        options.DefaultApiVersion = new ApiVersion(majorVersion: 1, minorVersion: 0);
+        options.ReportApiVersions = true;
+    });
 
-        builder.Services.AddApiVersioning(options =>
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo
         {
-            options.AssumeDefaultVersionWhenUnspecified = true;
-            options.DefaultApiVersion = new ApiVersion(majorVersion: 1, minorVersion: 0);
-            options.ReportApiVersions = true;
-        });
-
-
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen(options =>
-        {
-            options.SwaggerDoc("v1", new OpenApiInfo
+            Version = "v1",
+            Title = "Ecommerce",
+            Description = "Desenvolvimento de um mercado online de atacado e varejo de produtos",
+            Contact = new OpenApiContact()
             {
-                Version = "v1",
-                Title = "Ecommerce",
-                Description = "Desenvolvimento de um mercado online de atacado e varejo de produtos",
-                Contact = new OpenApiContact()
-                {
-                    Name = "Otávio Villas Boas Simoncini Carmanini",
-                    Email = "otaviovb.developer@gmail.com",
-                    Url = new Uri("https://github.com/otaviovb")
-                }
-            });
+                Name = "Otávio Villas Boas Simoncini Carmanini",
+                Email = "otaviovb.developer@gmail.com",
+                Url = new Uri("https://github.com/otaviovb")
+            }
         });
+    });
 
-        var app = builder.Build();
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-        app.UseHttpsRedirection();
-        app.UseAuthorization();
-        app.MapControllers();
-        app.Run();
+    var app = builder.Build();
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
     }
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
 }
