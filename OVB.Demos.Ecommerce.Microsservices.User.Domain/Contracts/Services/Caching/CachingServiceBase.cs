3bca172766d91ec657af944a3dfde0582455ba0e
OtavioVB <otaviovb.developer@gmail.com>
2022-10-29 11:42:36 -0300

    feat: caching service

---
+++ OVB.Demos.Ecommerce.Microsservices.User.Domain/Contracts/Services/Caching/CachingServiceBase.cs
@@ -0,0 +1,37 @@
+﻿using OVB.Demos.Ecommerce.Microsservices.User.Domain.Models.ValueObjects.ENUMs;
+using System.Text;
+
+namespace OVB.Demos.Ecommerce.Microsservices.User.Domain.Contracts.Services.Caching;
+
+public abstract class CachingServiceBase
+{
+    private string IdentifierValue { get; }
+    private string Description { get; }
+    private TypeCachingTimeService TypeCachingTimeService { get; }
+    private TypeCachingService TypeCachingService { get; }
+    public int Time { get; private set; }
+
+    protected CachingServiceBase(Guid identifier, TypeCachingTimeService typeCachingTimeService, TypeCachingService typeCachingService, string description)
+    {
+        IdentifierValue = identifier.ToString();
+        TypeCachingTimeService = typeCachingTimeService;
+        TypeCachingService = typeCachingService;
+        CreateCachingTime();
+        Description = description;
+    }
+
+    public virtual string GetCachingServiceDeclarationDescription()
+    {
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append(IdentifierValue);
+        stringBuilder.Append("_");
+        stringBuilder.Append("RequestService_");
+        stringBuilder.Append(Description);
+        return stringBuilder.ToString();
+    }
+
+    protected virtual void CreateCachingTime()
+    {
+        Time = 2;
+    }
+}
