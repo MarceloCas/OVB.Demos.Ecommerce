a95bfbb6f6d257148699ed4e8cbc2ed74abe0207
OtavioVB <otaviovb.developer@gmail.com>
2022-10-29 08:35:40 -0300

    feat(validation): `Fluent Validation`

--- OVB.Demos.Ecommerce.Microsservices.User.API/Program.cs
+++ OVB.Demos.Ecommerce.Microsservices.User.API/Program.cs
@@ -1,48 +1,54 @@
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Models;
+using OVB.Demos.Ecommerce.Microsservices.User.Domain.Models.DTOs.User;
+using OVB.Demos.Ecommerce.Microsservices.User.Domain.Models.Validators;
 
 namespace OVB.Demos.Ecommerce.Microsservices.User.Api;
 
 public class Program
 {
-public static void Main(string[] args)
-{
-    var builder = WebApplication.CreateBuilder(args);
-    builder.Services.AddControllers();
-
-    builder.Services.AddApiVersioning(options =>
+    public static void Main(string[] args)
     {
-        options.AssumeDefaultVersionWhenUnspecified = true;
-        options.DefaultApiVersion = new ApiVersion(majorVersion: 1, minorVersion: 0);
-        options.ReportApiVersions = true;
-    });
+        var builder = WebApplication.CreateBuilder(args);
+    
+        builder.Services.AddControllers();
 
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen(options =>
-    {
-        options.SwaggerDoc("v1", new OpenApiInfo
+        builder.Services.AddApiVersioning(options =>
         {
-            Version = "v1",
-            Title = "Ecommerce",
-            Description = "Desenvolvimento de um mercado online de atacado e varejo de produtos",
-            Contact = new OpenApiContact()
-            {
-                Name = "Otávio Villas Boas Simoncini Carmanini",
-                Email = "otaviovb.developer@gmail.com",
-                Url = new Uri("https://github.com/otaviovb")
-            }
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.DefaultApiVersion = new ApiVersion(majorVersion: 1, minorVersion: 0);
+            options.ReportApiVersions = true;
         });
-    });
 
-    var app = builder.Build();
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
+        builder.Services.AddTransient<IValidator<UserBase>, UserValidator>();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "Ecommerce",
+                Description = "Desenvolvimento de um mercado online de atacado e varejo de produtos",
+                Contact = new OpenApiContact()
+                {
+                    Name = "Otávio Villas Boas Simoncini Carmanini",
+                    Email = "otaviovb.developer@gmail.com",
+                    Url = new Uri("https://github.com/otaviovb")
+                }
+            });
+        });
+
+        var app = builder.Build();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.Run();
     }
-    app.UseHttpsRedirection();
-    app.UseAuthorization();
-    app.MapControllers();
-    app.Run();
-}
 }
---
+++ OVB.Demos.Ecommerce.Microsservices.User.Domain/Models/Validators/UserValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using OVB.Demos.Ecommerce.Microsservices.User.Domain.Models.DTOs.User;
+using System.Text.RegularExpressions;
+
+namespace OVB.Demos.Ecommerce.Microsservices.User.Domain.Models.Validators;
+
+public class UserValidator : AbstractValidator<UserBase>
+{
+    public UserValidator()
+    {
+        RuleFor(p => p.Identifier).NotEmpty().WithMessage("The identifier is required.");
+        RuleFor(p => p.Identifier).NotNull().WithMessage("The identifier is required.");
+        RuleFor(p => p.Identifier.ToString()).Matches(@"^[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{12}$").WithMessage("The identifier needs to be correct form.");
+    }
+}
